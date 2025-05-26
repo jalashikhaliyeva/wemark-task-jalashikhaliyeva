b1087989a863798468a67484fa577b8b879ec677
Jala Sh <jala.shikhaliyeva@gmail.com>
2025-05-27 02:23:19 +0400

    Add Header, Topbar , Hero and Service cards components with working toggle switcher of dark mode and responsive styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
+// pages/_app.tsx
+import { DarkModeProvider } from "@/src/shared/context/DarkModeContext";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <DarkModeProvider>
+      <Component {...pageProps} />
+    </DarkModeProvider>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,51 @@
+// pages/index.tsx
+import Container from "@/src/components/layout/Container";
+import Header from "@/src/components/layout/Header";
+import type { HeroData, ServiceFeatureData } from "@/src/types";
+import { getHeroData } from "./api/services/heroService";
+import Hero from "@/src/components/home/Hero";
+import TopToolBar from "@/src/components/layout/TopToolbar";
+import Services from "@/src/components/home/Services";
+import { getServiceFeatures } from "./api/services/servicesService";
 
+interface HomeProps {
+  heroData: HeroData;
+  serviceFeatures: ServiceFeatureData;
+}
 
-export default function Home() {
+export default function Home({ heroData, serviceFeatures }: HomeProps) {
   return (
- <>
-
-
- </>
+    <main>
+      <Container>
+        <Header />
+        <TopToolBar />
+        <Hero data={heroData} />
+        <Services data={serviceFeatures} />
+      </Container>
+    </main>
   );
+}
+
+export async function getServerSideProps() {
+  try {
+    const [heroData, serviceFeatures] = await Promise.all([
+      getHeroData(),
+      getServiceFeatures(),
+    ]);
+
+    return {
+      props: {
+        heroData,
+        serviceFeatures,
+      },
+    };
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return {
+      props: {
+        heroData: [],
+        serviceFeatures: [],
+      },
+    };
+  }
 }
